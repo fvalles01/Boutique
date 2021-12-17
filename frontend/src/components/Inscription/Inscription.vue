353f74b29295c29603dd4ea326c20f471de61f47
Arnaud-Lyard <contact@prochainweb.com>
2021-12-17 09:10:29 +0100

    inscription

--- frontend/src/components/Inscription/Inscription.vue
+++ frontend/src/components/Inscription/Inscription.vue
@@ -1,6 +1,6 @@
 <template>
   
-<form v-on:submit="inscription">
+<form v-on:submit.prevent="inscription">
   <div class="container">
     <div class="row justify-content-center">
     <div class="col12">
@@ -23,7 +23,6 @@
 </template>
 
 <script>
-    // import router from "../router"    
     import axios from "axios"   
     export default {    
         name: "Inscription",
@@ -33,30 +32,15 @@
           password:""
         }
       },   
-        methods: {    
-           inscription: (e) => {    
-                e.preventDefault()    
-                let email = this
-                console.log(email)
-                let password = this 
-                let inscription = () => {    
-                    let data = {    
-                        email: email,    
-                        password: password    
-                    }    
-                    axios.post("http://localhost:3000/api/auth/signup", data)    
-                        // .then((response) => {    
-                        //     console.log("Inscription OK")    
-                        //     router.push("/dashboard")    
-                        // })    
-                        // .catch((errors) => {    
-                        //     console.log("Inscription ECHEC")    
-                        // })    
-                }    
-                inscription()    
+           methods: {
+                    inscription() {
+                      axios.post("http://localhost:3000/api/auth/signup", {
+                        email: this.email,
+                        password: this.password,
+                      });
+                    },
+                  },  
             }    
-        }    
-    }
 </script>
 
 <style>
