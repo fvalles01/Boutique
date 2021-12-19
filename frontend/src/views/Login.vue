d649609f0f574891059450f65d50c5b516460175
Arnaud01500 <arnaud.lyard@gmail.com>
2021-12-19 18:42:13 +0100

    login

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -6,38 +6,35 @@
         <a class="nav-link"><router-link to="/login">Espace utilisateur</router-link></a>
         </nav>
     </div>
-
+  <div class="container">
   <div class="card">
     <h1 class="card__title" v-if="mode == 'login'">Connexion</h1>
     <h1 class="card__title" v-else>Inscription</h1>
     <p class="card__subtitle" v-if="mode == 'login'">Tu n'as pas encore de compte ? <span class="card__action" @click="switchToCreateAccount()">Créer un compte</span></p>
     <p class="card__subtitle" v-else>Tu as déjà un compte ? <span class="card__action" @click="switchToLogin()">Se connecter</span></p>
-    <div class="form-row">
+    <div class="form-row_position">
       <input v-model="email" class="form-row__input" type="text" placeholder="Adresse mail"/>
     </div>
-    <div class="form-row" v-if="mode == 'create'">
-      <input v-model="prenom" class="form-row__input" type="text" placeholder="Prénom"/>
-      <input v-model="nom" class="form-row__input" type="text" placeholder="Nom"/>
-    </div>
-    <div class="form-row">
+    <div class="form-row_position">
       <input v-model="password" class="form-row__input" type="password" placeholder="Mot de passe"/>
     </div>
-    <div class="form-row" v-if="mode == 'login' && status == 'error_login'">
+    <div class="form-row_position" v-if="mode == 'login' && status == 'error_login'">
       Adresse mail et/ou mot de passe invalide
     </div>
-    <div class="form-row" v-if="mode == 'create' && status == 'error_create'">
+    <div class="form-row_position" v-if="mode == 'create' && status == 'error_create'">
       Adresse mail déjà utilisée
     </div>
-    <div class="form-row">
-      <button @click="login()" class="button" :class="{'button--disabled' : !validatedFields}" v-if="mode == 'login'">
+    <div class="form-row_position">
+      <button @click="login()" class="btn btn-primary" :class="{'button--disabled' : !validatedFields}" v-if="mode == 'login'">
         <span v-if="status == 'loading'">Connexion en cours...</span>
         <span v-else>Connexion</span>
       </button>
-      <button @click="createAccount()" class="button" :class="{'button--disabled' : !validatedFields}" v-else>
+      <button @click="createAccount()" class="btn btn-primary" :class="{'button--disabled' : !validatedFields}" v-else>
         <span v-if="status == 'loading'">Création en cours...</span>
         <span v-else>Créer mon compte</span>
       </button>
     </div>
+  </div>
   </div>
   </div>
 </template>
@@ -117,28 +114,31 @@ export default {
 </script>
 
 <style>
-  .form-row {
-    display: flex;
-    margin: 16px 0px;
-    gap:16px;
-    flex-wrap: wrap;
-  }
+.form-row_position {
+  display: flex;
+  margin: 16px 0px;
+  gap:16px;
+  flex-wrap: wrap;
+}
 
-  .form-row__input {
-    padding:8px;
-    border: none;
-    border-radius: 8px;
-    background:#f2f2f2;
-    font-weight: 500;
-    font-size: 16px;
-    flex:1;
-    min-width: 100px;
-    color: black;
-  }
+.form-row__input {
+  padding:8px;
+  border: none;
+  border-radius: 8px;
+  background:#f2f2f2;
+  font-weight: 500;
+  font-size: 16px;
+  flex:1;
+  min-width: 100px;
+  color: black;
+}
 
-  .form-row__input::placeholder {
-    color:#aaaaaa;
-  }
-
+.form-row__input::placeholder {
+  color:#aaaaaa;
+}
+.card__action{
+  color: rgb(0, 119, 230);
+  cursor: pointer;
+}
 
 </style>
