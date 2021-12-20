b846bb0e2fd69bf94d072066169062fe16d86642
Arnaud-Lyard <contact@prochainweb.com>
2021-12-20 17:00:03 +0100

    modif

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const productSchema = mongoose.Schema({
-    designation:{type: String, required: true },
-    price: { type: Number, required: true },
-    imageUrl: { type: String, required: true },
-    userId: { type: String, required: true },
-    description: { type: String, required: true },
+const productSchema = Schema({
+    'designation':{type: String, required: true },
+    'price': { type: Number, required: true },
+    'imageUrl': { type: String, required: true },
+    'userId': { 
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    'description': { type: String, required: true },
 });
 
 module.exports = mongoose.model('Product', productSchema);
