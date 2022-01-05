097b27718a8a772e481e7d4c721e094fea1289ee
Arnaud-Lyard <contact@prochainweb.com>
2022-01-05 16:56:26 +0100

    fonctionnel

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,9 @@ const http = require('http');
 // const app = require('./app');
 const express = require('express');
 const bodyParser = require('body-parser');
-// const mongoose = require('mongoose');
 const path = require ('path');
 const dotenv = require('dotenv').config();
 
-const productRoutes = require('./routes/product');
-const userRoutes = require('./routes/user');
 const cors = require("cors");
 
 
@@ -124,12 +121,8 @@ app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
-// app.use(bodyParser.json());
-
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
-// app.use('/api/product', productRoutes);
-// app.use('/api/auth', userRoutes);
 
 // routes
 require('./routes/auth.routes')(app);
